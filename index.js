148011cfd5e625f958b8598382fd00a3ae9d10d0
trevor dewindt <trevor.dewindt@gmail.com>
2022-09-04 20:20:38 -0600

    add clear score event listener

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const init = () => {
+    populateSeasonDropDown();
+    let score = 0;
+    document.getElementById('score').textContent = score;
+    document.getElementById('main-game-form').addEventListener('submit', handleForm);
+    document.getElementById('clear-score-button').addEventListener('click', e => {
+        score = 0;
+        document.getElementById('score').textContent = score;
+    })
+
     const getCurrentDate = () => {
         const date = new Date();
         const year = date.getFullYear().toString();
@@ -30,15 +39,13 @@ const init = () => {
     function handleForm(e){
         e.preventDefault();
         const submitButton = document.getElementById('submit-button');
-        submitButton.disabled = true
-        setTimeout(() => submitButton.disabled = false, 300)
-        if(document.getElementById('card-container')) document.getElementById('card-container').remove();  //SAVE
-        // if(document.querySelectorAll('p')) document.querySelectorAll('p').forEach(element => element.remove()) //SAVE
-
+        submitButton.disabled = true;
+        setTimeout(() => submitButton.disabled = false, 300);
+        if(document.getElementById('card-container')) document.getElementById('card-container').remove();
         const dropDownSelection = document.getElementById('season-dropdown').value;
         if(dropDownSelection === '') {
             submitButton.disabled = false;
-            return alert('Oops! You need to choose a valid season.')
+            return alert('Oops! You need to choose a valid season.');
         }
         chooseTeamsOnSubmit(dropDownSelection);
     }
@@ -52,10 +59,10 @@ const init = () => {
         fetch(seasonEndPoint)
         .then(res => res.json())
         .then(data => {
-            const selectionContainer = document.getElementById('selection-container')
-            const cardContainer = document.createElement('div')
-            cardContainer.id = 'card-container'
-            cardContainer.className = 'container'
+            const selectionContainer = document.getElementById('selection-container');
+            const cardContainer = document.createElement('div');
+            cardContainer.id = 'card-container';
+            cardContainer.className = 'container';
 
             const selectionTable = createTable();
 
@@ -66,22 +73,21 @@ const init = () => {
             chosenTeams.forEach(element => chosenTeamData.push(retrieveTeamData(data, element, chosenStat)));
 
             setTimeout(() => {
-                document.getElementById('cell-11').textContent = chosenTeamData[0].team
-                document.getElementById('cell-21').textContent = chosenTeamData[0].statVal
-                document.getElementById('cell-22').textContent = chosenTeamData[0].stat
-                document.getElementById('cell-13').textContent = chosenTeamData[1].team
-                document.getElementById('cell-23').textContent = chosenTeamData[1].statVal
+                document.getElementById('cell-11').textContent = chosenTeamData[0].team;
+                document.getElementById('cell-21').textContent = chosenTeamData[0].statVal;
+                document.getElementById('cell-22').textContent = chosenTeamData[0].stat;
+                document.getElementById('cell-13').textContent = chosenTeamData[1].team;
+                document.getElementById('cell-23').textContent = chosenTeamData[1].statVal;
             }, 100)
             
-            cardContainer.appendChild(selectionTable)
-            selectionContainer.appendChild(cardContainer)
+            cardContainer.appendChild(selectionTable);
+            selectionContainer.appendChild(cardContainer);
 
+            const btnRight = document.getElementById('button-left');
+            const btnLeft = document.getElementById('button-right');
 
-            const btnRight = document.getElementById('button-left')
-            const btnLeft = document.getElementById('button-right')
-
-            btnLeft.addEventListener('click', handleTeamButton)
-            btnRight.addEventListener('click', handleTeamButton)
+            btnLeft.addEventListener('click', handleTeamButton);
+            btnRight.addEventListener('click', handleTeamButton);
         })
     }
 
@@ -94,32 +100,32 @@ const init = () => {
         document.getElementById('card-table').remove();
 
         if(leftStat === rightStat) {
-            cardContainer.textContent = 'It\'s a tie! Free point! Try another!'
+            cardContainer.textContent = 'It\'s a tie! Free point! Try another!';
         } else if (isLeftButton){
             if(leftStat > rightStat) {
-                score = score+=1
-                scoreCounter.textContent = score
-                cardContainer.textContent = 'Correct! Try another!'
+                score = score+=1;
+                scoreCounter.textContent = score;
+                cardContainer.textContent = 'Correct! Try another!';
             } else {
-                cardContainer.textContent = 'Oh no! That\'s wrong! Try another!'
+                cardContainer.textContent = 'Oh no! That\'s wrong! Try another!';
             }
         } else {
             if(rightStat > leftStat) {
-                score = score+=1
-                scoreCounter.textContent = score
-                cardContainer.textContent = 'Correct! Try another!'
+                score = score+=1;
+                scoreCounter.textContent = score;
+                cardContainer.textContent = 'Correct! Try another!';
             } else {
-                cardContainer.textContent = 'Oh no! That\'s wrong! Try another!'
+                cardContainer.textContent = 'Oh no! That\'s wrong! Try another!';
             }
         }
     }
 
     function createTable(){
-        const tableElement = document.createElement('table')
-        tableElement.id = 'card-table'
+        const tableElement = document.createElement('table');
+        tableElement.id = 'card-table';
         
         for(let i = 1; i < 4; i++){
-            buildTableStructure(i, tableElement)
+            buildTableStructure(i, tableElement);
         }
 
         return tableElement;
@@ -127,28 +133,30 @@ const init = () => {
 
 
     function buildTableStructure(iterator, tableElement){
-        const tr = document.createElement('tr')
-        const buttonLeft = document.createElement('button')
-        const buttonRight = document.createElement('button')
-        buttonLeft.id = 'button-left'
-        buttonLeft.textContent = 'This Team!'
-        buttonRight.id = 'button-right'
-        buttonRight.textContent = 'No, This Team!'
+        const tr = document.createElement('tr');
+        const buttonLeft = document.createElement('button');
+        const buttonRight = document.createElement('button');
+        buttonLeft.id = 'button-left';
+        buttonLeft.className = 'button';
+        buttonLeft.textContent = 'This Team!';
+        buttonRight.id = 'button-right';
+        buttonRight.className = 'button';
+        buttonRight.textContent = 'No, This Team!';
 
         for(let j = 1; j < 4; j++){
-            const td = document.createElement('td')
-            td.id = `cell-${iterator}${j}`
+            const td = document.createElement('td');
+            td.id = `cell-${iterator}${j}`;
 
             if(td.id === 'cell-31'){
-                td.appendChild(buttonLeft)
+                td.appendChild(buttonLeft);
             } else if(td.id === 'cell-33'){
-                td.appendChild(buttonRight)
+                td.appendChild(buttonRight);
             }
 
-            tr.appendChild(td)
+            tr.appendChild(td);
         }
 
-        tableElement.appendChild(tr)
+        tableElement.appendChild(tr);
     }
 
 
@@ -168,7 +176,7 @@ const init = () => {
     }
 
     function chooseRandomValue(arr){
-        return arr[Math.floor(Math.random() * arr.length)]
+        return arr[Math.floor(Math.random() * arr.length)];
     }
 
     // checks which team the id is attached to and pulls the name and wins for that team/season
@@ -189,13 +197,6 @@ const init = () => {
         }))
         return teamObj;
     }
-
-    let score = 0;
-    document.getElementById('score').textContent = score;
-    populateSeasonDropDown();
-    const gameForm = document.getElementById('main-game-form');
-    gameForm.addEventListener('submit', handleForm);
-
 }
 
 addEventListener('DOMContentLoaded', init);
